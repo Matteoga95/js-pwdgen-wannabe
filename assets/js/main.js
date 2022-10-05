b1c624771f4be2d498fd5141cb0d9bf53d8f3987
matteoga95 <gallo.work95@gmail.com>
2022-10-05 14:58:06 +0200

    script inserito

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+const userFirstName = prompt("Inserisci il tuo nome");
+const userLastName = prompt("Inserisci il tuo cognome");
+const userColor = prompt("Inserisci il tuo colore preferito");
+
+document.getElementById("message").innerHTML=`${userFirstName}${userLastName}${userColor}`;
